56ac078163dd0e42e3f2b8b1d25234506647dea2
Noah Rondeau <ntrondeau@gmail.com>
2018-11-01 01:58:24 -0700

    Finished Code
    Completed all the functionality including initialization, button sensing, motor rotation and positioning algo, light sequence, etc.

--- Halloween2018.ino
+++ Halloween2018.ino
@@ -2,13 +2,19 @@
 
 #include <Servo.h>
 #define MOTOR_PIN (9)
-#define MOTOR_START_LIMIT (150) // all the way counter clockwise
-#define MOTOR_END_LIMIT (5) // all the way clockwise
-#define MOTOR_STEP_SIZE (20)
+#define MOTOR_START_LIMIT (0) // all the way counter clockwise
+#define MOTOR_END_LIMIT (162) // all the way clockwise
+#define MOTOR_STEP_SIZE (21)
 
-#define BUTTON1 (2)
-#define BUTTON2 (3)
-#define BUTTON_PRESSED (1)
+#define MOTOR_READ_PIN (A0)
+
+#define BUTTON1 (13)
+#define BUTTON2 (2)
+#define BUTTON_PRESSED (HIGH)
+
+#define LIGHTLEN (9)
+
+const int lights[] = {3,4,5,6,7,8,10,11,12};
 
 
 Servo motor;
@@ -18,14 +24,14 @@ void startupSequence();
 void waitButtons();
 void lightShow();
 void dropNextMatch();
+void lightWrite();
+void turnOffLights();
+void deniedLights();
 
-// we go clockwise
+// we go counter clockwise
 bool endReached = false;
 int match_count = 0;
 int current_angle;
-
-
-
 
 void setup() {
   motor.attach(MOTOR_PIN);
@@ -34,6 +40,9 @@ void setup() {
   pinMode(BUTTON1, INPUT);
   pinMode(BUTTON2, INPUT);
 
+  for( int i = 0; i <= LIGHTLEN; i++)
+    pinMode(lights[i], OUTPUT);
+
   delay(2000);
   startupSequence();
   delay(2000);
@@ -41,30 +50,37 @@ void setup() {
 }
 
 void loop() {
-  Serial.print("Motor Angle: ")
-  Serial.println(angle);
 
   waitButtons();
   delay(1000);
   lightShow();
   delay(1000);
-  dropNextMatch();
+  if (random(0,8) ==4 )
+    dropNextMatch();
+  else
+    deniedLights();
+  delay(2000);
+  turnOffLights();
 }
 
 void startupSequence()
 {
-  // nothing now
+  motor.write(MOTOR_START_LIMIT);
 }
 
 void waitButtons()
 {
-   
+  while (digitalRead(BUTTON1) != BUTTON_PRESSED && digitalRead(BUTTON2) != BUTTON_PRESSED);
+    delay(2000);
+  while(true)
+  {
+   if(digitalRead(BUTTON1) != BUTTON_PRESSED && digitalRead(BUTTON2) != BUTTON_PRESSED)
+    return;
+  }
 }
 
-void dropNextMatch();
+void dropNextMatch()
 {
-  if (!endReached)
-  {
     if ( match_count == 0)
     {
       motor.write(MOTOR_START_LIMIT);
@@ -78,12 +94,145 @@ void dropNextMatch();
       current_angle += MOTOR_STEP_SIZE;
       motor.write(current_angle);
       match_count++;
+    }
     
     if (match_count == 8)
     {
       // we just did the last one, now lets move to the start pos
       motor.write(MOTOR_START_LIMIT);
-      
+      match_count = 0;
+
+    }
+}
+
+void lightShow()
+{
+  for( int j = 0; j < 20; j++)
+  {
+    for ( int i = 0; i < 20; i++)
+      {
+      lightWrite(i, HIGH);
+      delay(20);
+    }
+
+    for ( int i = 0; i < 20; i++)
+    {
+      lightWrite(i, LOW);
+      delay(20);
     }
   }
+
+  lightWrite(0, HIGH);
+  lightWrite(1, HIGH);
+  lightWrite(2, HIGH);
+  lightWrite(3, HIGH);
+  lightWrite(4, HIGH);
+  lightWrite(5, HIGH);
+  lightWrite(6, HIGH);
+  lightWrite(7, HIGH);
+  lightWrite(8, HIGH);
+
+  delay(500);
+
+  lightWrite(0, LOW);
+  lightWrite(1, LOW);
+  lightWrite(2, LOW);
+  lightWrite(3, LOW);
+  lightWrite(4, LOW);
+  lightWrite(5, LOW);
+  lightWrite(6, LOW);
+  lightWrite(7, LOW);
+  lightWrite(8, LOW);
+
+  delay(500);
+
+    lightWrite(0, HIGH);
+  lightWrite(1, HIGH);
+  lightWrite(2, HIGH);
+  lightWrite(3, HIGH);
+  lightWrite(4, HIGH);
+  lightWrite(5, HIGH);
+  lightWrite(6, HIGH);
+  lightWrite(7, HIGH);
+  lightWrite(8, HIGH);
+
+  delay(500);
+
+  lightWrite(0, LOW);
+  lightWrite(1, LOW);
+  lightWrite(2, LOW);
+  lightWrite(3, LOW);
+  lightWrite(4, LOW);
+  lightWrite(5, LOW);
+  lightWrite(6, LOW);
+  lightWrite(7, LOW);
+  lightWrite(8, LOW);
+
+  delay(500);
+  
+  lightWrite(0, HIGH);
+  lightWrite(1, HIGH);
+  lightWrite(2, HIGH);
+  lightWrite(3, HIGH);
+  lightWrite(4, HIGH);
+  lightWrite(5, HIGH);
+  lightWrite(6, HIGH);
+  lightWrite(7, HIGH);
+  lightWrite(8, HIGH);
+
+  delay(500);
+
+  lightWrite(0, LOW);
+  lightWrite(1, LOW);
+  lightWrite(2, LOW);
+  lightWrite(3, LOW);
+  lightWrite(4, LOW);
+  lightWrite(5, LOW);
+  lightWrite(6, LOW);
+  lightWrite(7, LOW);
+  lightWrite(8, LOW);
+
+  delay(500);
+
+  
+  lightWrite(0, HIGH);
+  lightWrite(1, HIGH);
+  lightWrite(2, HIGH);
+  lightWrite(3, HIGH);
+  lightWrite(4, HIGH);
+  lightWrite(5, HIGH);
+  lightWrite(6, HIGH);
+  lightWrite(7, HIGH);
+  lightWrite(8, HIGH);
+}
+
+void lightWrite( int index, int val)
+{
+  digitalWrite(lights[index], val);
+}
+
+void turnOffLights()
+{
+    lightWrite(0, LOW);
+  lightWrite(1, LOW);
+  lightWrite(2, LOW);
+  lightWrite(3, LOW);
+  lightWrite(4, LOW);
+  lightWrite(5, LOW);
+  lightWrite(6, LOW);
+  lightWrite(7, LOW);
+  lightWrite(8, LOW);
+}
+
+void deniedLights()
+{
+  lightWrite(0, LOW);
+  lightWrite(1, HIGH);
+  lightWrite(2, LOW);
+  lightWrite(3, HIGH);
+  lightWrite(4, LOW);
+  lightWrite(5, HIGH);
+  lightWrite(6, LOW);
+  lightWrite(7, HIGH);
+  lightWrite(8, LOW);
 }
